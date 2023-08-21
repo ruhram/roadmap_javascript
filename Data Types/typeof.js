c5ef5a16cce381f0cd6961862e40047d2b1194fb
Rully Gandadiwirya <rullyrgandadiwirya@gmail.com>
2023-08-21 10:01:11 +0700

    added data types

---
+++ Data Types/typeof.js
@@ -0,0 +1,11 @@
+console.log(typeof 42);
+// Expected output: "number"
+
+console.log(typeof 'blubber');
+// Expected output: "string"
+
+console.log(typeof true);
+// Expected output: "boolean"
+
+console.log(typeof undeclaredVariable);
+// Expected output: "undefined"
