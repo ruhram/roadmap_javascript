9eab106b0591cc52416dd39e8d2e2bc9a21a120c
Rully Gandadiwirya <rullyrgandadiwirya@gmail.com>
2023-08-30 22:59:44 +0700

    Added iterator

---
+++ iterators.js
@@ -0,0 +1,16 @@
+//basic for loops
+
+var nums = [1,5,16];
+
+for (var i = 0; i < nums.length; i++){
+    console.log(nums[i]);
+}
+
+//output : 1 5 16
+
+//for...of loop
+var nums = [1,5,16];
+
+for(var num of nums){
+    console.log(num);
+}
